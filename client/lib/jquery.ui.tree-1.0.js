d34121c1e8b6862a3fa795e49e7f07621be688e4
Tobias Klemmer <tobias@klemmer.info>
2014-03-10 19:09:15 +0100

    V 0.7.1
    without comments

--- client/lib/jquery.ui.tree-1.0.js
+++ client/lib/jquery.ui.tree-1.0.js
@@ -275,7 +275,6 @@
                 }
                 Session.set('selectedNodes', selectedNodes);
                 Session.set('selectedTree' , selectedTree );
-                console.log("changed.jstree:", selectedTree, selectedNodes);
             });
         },
 
